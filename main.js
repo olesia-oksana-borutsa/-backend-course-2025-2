c08f84e6afda892f7eb8e3754b5447ba6f5a569c
Olesia bc2025-2 <OLESIA-OKSANA.BORUTSA@lnu.edu.ua>
2025-09-14 15:36:31 +0300

    added code in main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+const {URL, URLSearchParams} = require("url");
+const currency = "USD";
+const date_from = "20220707";
+const date_to = "20220719";
+
+const base = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange";
+//url for first
+const url=new URL(base);
+url.search = new URLSearchParams ({
+    valcode: currency,
+    date: date_from,
+    json: " "
+});
+
+console.log("URL для запиту:", url.toString());
