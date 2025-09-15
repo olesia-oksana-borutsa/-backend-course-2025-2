99963ce74e9cdcb03186f02fc17514794043c114
Olesia bc2025-2 <OLESIA-OKSANA.BORUTSA@lnu.edu.ua>
2025-09-15 15:19:55 +0300

    changed to old one

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const {URL, URLSearchParams} = require("url");
+const { URL, URLSearchParams } = require('url');
+
 const currency = "USD";
 const date_from = "20220707";
 const date_to = "20220719";
 
 const base = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange";
-//url for first
-const url=new URL(base);
-url.search = new URLSearchParams ({
-    start: date_from,
- end: date_to,
-    valcode: currency,
-  
-});
-url.search +="&json";
+const url = new URL(base);
+
+
+url.search = new URLSearchParams({
+  valcode: currency,
+  date: date_from
+}).toString() + "&json";
+
 console.log("URL для запиту:", url.toString());
